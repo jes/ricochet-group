28455d01087644f90fc1ca63935ef564bd28cf3c
James Stanley <james@incoherency.co.uk>
2018-09-27 00:56:01 +0100

    Rewrite to use ricochetbot library

--- group.go
+++ group.go
@@ -1,122 +1,49 @@
 package main
 
 import (
-  "github.com/s-rah/go-ricochet/application"
-  "github.com/s-rah/go-ricochet/channels"
-  "github.com/s-rah/go-ricochet/utils"
-  "log"
-  "time"
-  "fmt"
+	"fmt"
+	"github.com/jes/go-ricochet/utils"
+	"github.com/jes/ricochetbot"
+	"log"
 )
 
-type BigBoy struct {
-    bots []*ChatEchoBot
-}
-
-type ChatEchoBot struct {
-	rai      *application.ApplicationInstance
-    bigboy   *BigBoy
-}
-
-// We always want bidirectional chat channels
-func (bot *ChatEchoBot) OpenInbound() {
-	log.Println("OpenInbound() ChatChannel handler called...")
-	outboutChatChannel := bot.rai.Connection.Channel("im.ricochet.chat", channels.Outbound)
-	if outboutChatChannel == nil {
-		bot.rai.Connection.Do(func() error {
-			bot.rai.Connection.RequestOpenChannel("im.ricochet.chat",
-				&channels.ChatChannel{
-					Handler: bot,
-				})
-			return nil
-		})
+func SendToAll(bot *ricochetbot.RicochetBot, avoidPeer *ricochetbot.Peer, message string) {
+	for _, p := range bot.Peers {
+		if p != avoidPeer {
+			p.SendMessage(message)
+		}
 	}
 }
-
-func (bot *ChatEchoBot) ChatMessage(messageID uint32, when time.Time, message string) bool {
-	log.Printf("ChatMessage(from: %v, %v", bot.rai.RemoteHostname, message)
-    message = bot.rai.RemoteHostname + ": " + message
-    MessageEveryone(bot.bigboy, bot, message)
-	return true
-}
-
-// set "bot" to nil if it should go to everyone
-func MessageEveryone(bigboy *BigBoy, bot *ChatEchoBot, message string) {
-    for _, otherbot := range bigboy.bots {
-        if otherbot != bot {
-            SendMessage(otherbot.rai, message)
-        }
-    }
-}
-
-func SendMessage(rai *application.ApplicationInstance, message string) {
-	log.Printf("SendMessage(to: %v, %v)\n", rai.RemoteHostname, message)
-	rai.Connection.Do(func() error {
-
-		log.Printf("Finding Chat Channel")
-		channel := rai.Connection.Channel("im.ricochet.chat", channels.Outbound)
-		if channel != nil {
-			log.Printf("Found Chat Channel")
-			chatchannel, ok := channel.Handler.(*channels.ChatChannel)
-			if ok {
-				chatchannel.SendMessage(message)
-			}
-		} else {
-			log.Printf("Could not find chat channel")
-		}
-		return nil
-	})
-}
-
-func (bot *ChatEchoBot) ChatMessageAck(messageID uint32, accepted bool) {
-
-}
-
-
 
 func main() {
-  pk, err := utils.LoadPrivateKeyFromFile("./private_key")
-  if err != nil {
-    log.Fatalf("error reading private key file: %v", err)
-  }
-
-  bigboy := &BigBoy{bots: make([]*ChatEchoBot, 0)}
-
-
-	fmt.Println("Initializing application factory...")
-	af := application.ApplicationInstanceFactory{}
-	af.Init()
-
-	af.AddHandler("im.ricochet.contact.request", func(rai *application.ApplicationInstance) func() channels.Handler {
-		return func() channels.Handler {
-			contact := new(channels.ContactRequestChannel)
-			contact.Handler = new(application.AcceptAllContactHandler)
-			return contact
-		}
-	})
-
-	fmt.Println("Starting alice...")
-	alice := new(application.RicochetApplication)
-	fmt.Println("Generating alice's pk...")
-	aliceAddr, _ := utils.GetOnionAddress(pk)
-	fmt.Println("Seting up alice's onion " + aliceAddr + "...")
-	al, err := application.SetupOnion("127.0.0.1:9051", "tcp4", "", pk, 9878)
+	pk, err := utils.LoadPrivateKeyFromFile("./private_key")
 	if err != nil {
-		fmt.Printf("Could not setup Onion for Alice: %v", err)
-        return
+		log.Fatalf("error reading private key file: %v", err)
 	}
 
-	fmt.Println("Initializing alice...")
-	af.AddHandler("im.ricochet.chat", func(rai *application.ApplicationInstance) func() channels.Handler {
-		return func() channels.Handler {
-			chat := new(channels.ChatChannel)
-			thisbot := &ChatEchoBot{rai: rai, bigboy: bigboy}
-			chat.Handler = thisbot
-            bigboy.bots = append(bigboy.bots, thisbot)
-			return chat
-		}
-	})
-	alice.Init("Alice", pk, af, new(application.AcceptAllContactManager))
-	fmt.Println("Running alice...")
-	alice.Run(al)
+	bot := new(ricochetbot.RicochetBot)
+	bot.PrivateKey = pk
+
+	bot.OnConnect = func(peer *ricochetbot.Peer) {
+		fmt.Println("We connected to ", peer.Onion)
+	}
+	bot.OnNewPeer = func(peer *ricochetbot.Peer) bool {
+		fmt.Println(peer.Onion, " connected to us")
+		SendToAll(bot, peer, "*** "+peer.Onion+" has connected.")
+		return true // true == already-known contact
+	}
+	bot.OnMessage = func(peer *ricochetbot.Peer, message string) {
+		message = "<" + peer.Onion + "> " + message
+		SendToAll(bot, peer, message)
+	}
+	bot.OnContactRequest = func(peer *ricochetbot.Peer, name string, desc string) bool {
+		fmt.Println(peer.Onion, " wants to be our friend")
+		return true // true == accept
+	}
+	bot.OnDisconnect = func(peer *ricochetbot.Peer) {
+		fmt.Println(peer.Onion, " disconnected")
+		SendToAll(bot, peer, "*** "+peer.Onion+" has disconnected.")
+	}
+
+	bot.Run()
 }
